f00702c5eeeb9485187b28fb6a08bc6d3ce87de5
Varun Kolluru <varun@Varuns-MacBook-Air.local>
2025-03-10 23:06:20 +0530

    added sticky

--- app/page.js
+++ app/page.js
@@ -9,26 +9,28 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="relative bg-[#000319] text-white overflow-hidden">
+    <>
       <Navbar />
-      <div id="about">
-        <About />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="experience">
-        <Experience />
-      </div>
-      <div id="achievements">
-        <Achievements />
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-      <div id="connect">
-        <LampDemo />
-      </div>
-    </main>
+      <main className="relative bg-[#000319] text-white overflow-hidden">
+        <div id="about">
+          <About />
+        </div>
+        <div id="skills">
+          <Skills />
+        </div>
+        <div id="experience">
+          <Experience />
+        </div>
+        <div id="achievements">
+          <Achievements />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="connect">
+          <LampDemo />
+        </div>
+      </main>
+    </>
   );
 }
